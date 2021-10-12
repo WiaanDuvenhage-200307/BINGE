f003fc430005fe50d30354560f25565cd9cf3ca0
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2021-10-13 00:36:57 +0200

    Scrapped index.html and started over
    In this commit I completely scrapped everything and started over but started with desktop first.

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    const url = "https://api.themoviedb.org/3/discover/movie?api_key=2b5c4ad7d6c5ddafd1383a90b3aca91b&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=1";
+
+    const imagePrefix = "https://image.tmdb.org/t/p/original";
+
+    $.getJSON(url, result =>{
+        console.log(result);
+
+        result.results.forEach(i => {
+            console.log(i.poster_path);
+            $(".movie-cards").append(`
+
+                <div class="col-xxl-2 movie">
+                    // <img src=${imagePrefix + i.poster_path} alt="movie_card height="323px" width="100%">
+                </div>
+
+           `);
+        });
+
+    });
+
+    
+
+})
+
+// word => means function(word) 
