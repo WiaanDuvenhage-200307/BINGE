831457d0ded3dff7a5b106e9d3f957eb5985464c
Marco <21100161@virtualwindow.co.za>
2021-10-18 01:25:02 +0200

    watch-later page layout start; functionality on library pg
    Layout Started for watch later page;
    img, description and remove placement done;
    hover functionality in library page implemented

--- js/movie-library.js
+++ js/movie-library.js
@@ -1,34 +1,47 @@
 $(function(){
-
-    //on click function for filter buttons//
-    $(".filter-button").click(function(){
-        $(".filter-button").removeClass("filter-active");
-        $(this).addClass("filter-active");
-    });
-
     // Getting all the images of all the movies
     const url = "https://api.themoviedb.org/3/discover/movie?api_key=2b5c4ad7d6c5ddafd1383a90b3aca91b&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=1";
 
     const imagePrefix = "https://image.tmdb.org/t/p/original";
+    
 
-    $.getJSON(url, result =>{
-        console.log(result);
+    $.getJSON(url, function(result){
 
-        result.results.forEach(i => {
-            console.log(i.poster_path);
+        for( i = 0; i < 12; i++){
+
+
             $(".movie-cards").append(`
 
-                <div class="col-xxs-6 col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xxl-2 movie">
-                    <br> <a href="/pages/individual-movie.html"><img class="card" src=${imagePrefix + i.poster_path} alt="movie_card height="20%" width="100%"></a>
+                <div class="col-xxl-2 col-md-3 col-12 movie">
+                    <div class="overlay">
+                        <a href= '../pages/individual-movie.html?id='${result.results[i].id}'><img src="${imagePrefix + result.results[i].poster_path}" alt="movie_card height="323px" width="100%"></a>
+                        <div class="movie-details" style="text-align: center; opacity: 0;">
+        
+                            <h5>${result.results[i].title}</h5>
+                            <h2>${result.results[i].release_date}</h2>
+                            <h7>BINGE SCALE: ${result.results[i].vote_average}</h7>
+        
+                        </div>
+                    </div>
+                
                 </div>
+    
+               `);
 
-           `);
+            console.log(result.results[i].id);
+
+        }
+
+        $(".movie").mouseenter(function(){
+            $(this).find(".movie-details").css('opacity', '1')
+        });
+
+        $(".movie").mouseleave(function(){
+            $(this).find(".movie-details").css('opacity', '0')
         });
 
     });
 
-
-    // getting details of movie
 
     
 
