1092dca087c4ef4003ee3b7029eee190ec54922c
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2021-10-18 21:44:56 +0200

    Populated Individual Movie Page 50% Done

--- js/index.js
+++ js/index.js
@@ -1,34 +1,37 @@
 $(function(){
     // Getting all the images of all the movies
-    const url = "https://api.themoviedb.org/3/discover/movie?api_key=2b5c4ad7d6c5ddafd1383a90b3aca91b&certification_country=US&certification.lte=G&sort_by=popularity.desc&page=1";
+    const url = "https://api.themoviedb.org/3/movie/popular?api_key=2b5c4ad7d6c5ddafd1383a90b3aca91b&language=en-US&page=8";
 
     const imagePrefix = "https://image.tmdb.org/t/p/original";
+
+    
     
 
     $.getJSON(url, function(result){
 
         for( i = 0; i < 12; i++){
 
+            var year = result.results[i].release_date.split("-").slice(0, -2);
 
-            var card = $(".movie-cards").append(`
 
-                <div class="col-xxl-2 col-md-3 col-12 movie">
+            console.log(result);
+
+            var card = $(".movie-cards").append(
+                `<div class="col-xxl-2 col-md-3 col-12 movie">
                     <div class="overlay">
-                        <a href= '../pages/individual-movie.html?id='${result.results[i].id}'><img src="${imagePrefix + result.results[i].poster_path}" alt="movie_card height="323px" width="100%"></a>
+                        <a href= "../pages/individual-movie.html?id= `+ result.results[i].id +`"><img src="${imagePrefix + result.results[i].poster_path}" alt="movie_card height="323px" width="100%"></a>
                         <div class="movie-details" style="text-align: center;">
         
                             <h5>${result.results[i].title}</h5>
-                            <h2>${result.results[i].release_date}</h2>
+                            <h2>${year}</h2>
                             <h7>BINGE SCALE: ${result.results[i].vote_average}</h7>
         
                         </div>
                     </div>
                 
-                </div>
-    
-               `);
+                </div>`);
 
-            console.log(result.results[i].id);
+            
 
         }
 
--- js/individual-movie.js
+++ js/individual-movie.js
@@ -9,18 +9,78 @@ $(function(){
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get("id");
 
-    const url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=2b5c4ad7d6c5ddafd1383a90b3aca91b";
+    const url = "https://api.themoviedb.org/3/movie/"+id+"?api_key=2b5c4ad7d6c5ddafd1383a90b3aca91b";
+
+    const imagePrefix = "https://image.tmdb.org/t/p/original";
+
+    console.log(url);
 
     $.getJSON(url, function(result){
-        console.log(result);
+        
 
-        movie_image = result.backdrop_path;
+        movie_image = imagePrefix + result.backdrop_path;
         movie_title = result.title;
-        movie_genres = result.genre_ids;
-        movie_date = result.release_date;
+        movie_genre1 = result.genres[0].name;
+        movie_genre2 = result.genres[1].name;
+        movie_date =  result.release_date.split("-").slice(0, -2);
+        movie_rate = result.vote_average;
+        movie_desc = result.overview;
+        movie_duration = result.runtime;
+        
+        
 
-        console.log(movie_title);
+        $('.header-img').append(
+            "<div class='col-12 movie-img row'>\
+            <img src='"+ movie_image +"' alt='' height = '500px'/>\
+            </div>"
+        )
 
+        $(".movie-details").append(
+            "<h1>"+ movie_title +"</h1>\
+            <h2>"+ movie_genre1 + " | "+ movie_genre2 +"</h2>\
+            <h2>"+ movie_date +"</h2>\
+            <div class='rating'>\
+                <img src='../assets/icons/rating.svg' alt='rating' height='50px'>\
+                <h2>Rating: "+ movie_rate +"</h2>\
+            </div>"
+        )
+
+        $(".overview").append(
+            " <div class='body-text col-xxl-6 col-12'>\
+            <h3>"+ movie_desc +"</h3>\
+            </div>"
+        )
+
+        $(".runtime").append(
+            "<h2>"+ movie_duration +" mins</h2>\
+            "
+        )
+
+        if(movie_rate === 0){
+            $(".rating").text("NO RATING!")
+            $(".rating").css("font-family", 'cantarell');
+            $(".rating").css("color", 'white');
+            $(".rating").css("backgroundColor", '#D3418D');
+            $(".rating").css("width", '200px');
+            $(".rating").css("borderRadius", '100px');
+            $(".rating").css("padding", '10px');
+            $(".rating").css("text-align", 'center');
+        } else{
+            $(".rating").text(movie_rate);
+            $(".rating").css("font-family", 'Fjalla One');
+            $(".rating").css("font-size", '36px');
+            $(".rating").css("color", 'white');
+            $(".rating").css("width", '100px');
+            $(".rating").css("backgroundColor", '#D3418D');
+            $(".rating").css("borderRadius", '50px');
+            $(".rating").css("padding", '10px');
+            $(".rating").css("text-align", 'center');
+        }
+
+        if(movie_image === null){
+        } else{
+            movie_image;
+        }
     })
 
 })
