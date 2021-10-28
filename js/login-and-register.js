bf35b33fcd706a3eac3224a2f4192bd35ecac815
Marco <21100161@virtualwindow.co.za>
2021-10-28 16:49:56 +0200

    working on logreg pt1

--- js/login-and-register.js
+++ js/login-and-register.js
@@ -38,30 +38,78 @@
 // });  LITERALLY TRYING BUT NOT GETTING TO WHERE THE RESULTS ARE>>>>YET. 
 
 // FUNCTIONS:
+// $(function(){
+
+//     $('.btn prim mt-4').on("click", function () {
+// //globl variables .. outside of a function. we can access this anywhere now
+//         var username =$(".firstname").val();
+//         var password =$(".psw").val();
+
+//         url = "https//owmakerspace.co.za/user/data.json";
+
+//         $.getJSON(url, function (result) {
+//             console.log(result.users);
+
+//             for ( i=0; i < results.user.lenght; i++){//users..is the array, result is just the return obj {}
+// //some logic (nested if statements, primary if statement>>>checking for the user<<<)
+//                 if(result.user[i].username === username) {
+//                     console.log("The user exists");
+//                 } else //if we dont have such a person/user in the datavase
+//                     console.log("The user does not exist");
+
+//             } 
+
+
+//         }); //end of http request
+
+//     }); // end of click event
+
+// });
+
 $(function(){
 
-    $('.btn prim mt-4').on("click", function () {
-//globl variables .. outside of a function. we can access this anywhere now
-        var username =$(".firstname").val();
-        var password =$(".psw").val();
+    $(".log-in-btn").click(function(){
 
-        url = "https//owmakerspace.co.za/user/data.json";
+        var username = $(".username").val();
+        var password = $(".password").val();
+        url = "https://owmakerspace.co.za/users/data.json";
 
-        $.getJSON(url, function (result) {
+        $.getJSON(url, function(result){
             console.log(result.users);
 
-            for ( i=0; i < results.user.lenght; i++){//users..is the array, result is just the return obj {}
-//some logic (nested if statements, primary if statement>>>checking for the user<<<)
-                if(result.user[i].username === username) {
-                    console.log("The user exists");
-                } else //if we dont have such a person/user in the datavase
-                    console.log("The user does not exist");
 
-            } 
+            for(i=0; i < result.users.length; i++){
+
+                if(result.users[i].username === username){
+                    console.log("user exists");
+
+                    if(result.users[i].password === password){
+                        console.log("password match");
+
+                        if(result.users[i].account === "active"){
+                            window.location.href = "http://www.binge-movies.co.za";
+                            sessionStorage.setItem("username", username);
+                        } else{
+                            $(".message").css('background-color', 'tomato');
+                            alert("This account does not exist.");
+                            console.log('account suspended')
+                        }
+
+                    } else{
+                        $(".message").css('background-color', 'tomato');
+                        alert("Email or Password does not match");
+                    }
+                } else{
+                    $(".message").css('background-color', 'tomato');
+                    alert("Email or Password does not match");
+                }
+
+                
+            }
+        });
 
 
-        }); //end of http request
 
-    }); // end of click event
+    });//end of click function
 
 });
